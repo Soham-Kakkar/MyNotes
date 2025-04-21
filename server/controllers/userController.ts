8e08949da1a05fef3887af5e51fa5951f236a64a
Soham Kakkar <kakkarsoham@gmail.com>
2025-04-21 13:32:35 +0530

    quickfix to an issue with last fix

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -43,7 +43,7 @@ export const login = async (req: Request, res: Response) => {
     res.cookie('authToken', token, {
       httpOnly: true,
       maxAge: 5 * 24 * 60 * 60 * 1000,
-      sameSite: 'strict',
+      sameSite: 'none',
       secure: true,
     });
     res.json({ message: 'Logged in successfully' });
