295a0217a26c19d4815964d5bc329ecdd96f5770
Soham Kakkar <kakkarsoham@gmail.com>
2025-04-27 00:57:04 +0530

    Added rate limiter

---
+++ server/middleware/rateLimiter.ts
@@ -0,0 +1,19 @@
+import rateLimit from 'express-rate-limit';
+
+const userApiLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  limit: 20, // Limit each IP to 20 requests per windowMs
+  message: 'Too many requests from this IP, please try again later.',
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+const NotesApiLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  limit: 100, // Limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again later.',
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+export { userApiLimiter, NotesApiLimiter };
--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import connectDB from './config/db';
 import userRoutes from './routes/userRoutes';
 import noteRoutes from './routes/noteRoutes';
-import middleware from './middleware/middleware'
+import middleware from './middleware/middleware';
+import { userApiLimiter, NotesApiLimiter } from './middleware/rateLimiter';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,8 +12,8 @@ connectDB();
 
 app.use(...middleware);
 
-app.use('/api/users', userRoutes);
-app.use('/api/notes', noteRoutes);
+app.use('/api/users', userApiLimiter, userRoutes);
+app.use('/api/notes', NotesApiLimiter, noteRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
